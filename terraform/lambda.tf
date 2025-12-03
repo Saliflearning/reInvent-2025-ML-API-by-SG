b73caaa628bcf7d73260e63a079d01f1a71b086c
Saliflearning <Saliflearning@gmail.com>
2025-12-03 10:55:18 -0800

    feat: add lambda terraform resources

---
+++ terraform/lambda.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "sg-reinvent-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "sentiment_lambda" {
+  function_name = "sg-reinvent-sentiment-lambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "app.lambda_handler"
+  runtime       = "python3.10"
+
+  # For now this points to a zip we will create later
+  filename         = "${path.module}/../lambda/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambda/lambda.zip")
+}
